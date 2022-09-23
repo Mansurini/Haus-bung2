fb5167816384a75034373d16cec4f99ca8e35917
manfred nacktname <mansurgewinner@gmail.com>
2022-09-23 12:28:20 +0200

    Lambda ausdruck und liste ausgeben in main

--- Hausuebung2/src/HalloJavaForEach.java
+++ Hausuebung2/src/HalloJavaForEach.java
@@ -3,10 +3,16 @@ import java.util.List;
 public class HalloJavaForEach {
     List <String> liste;
 
+    public HalloJavaForEach(List<String> liste) {
+        this.liste = liste;
+    }
+
     public void listeausgeben()
     {
         for (String zeile : liste) {
             System.out.println(zeile);
         }
+
+        liste.forEach((String s) -> System.out.println(s));
     }
 }
