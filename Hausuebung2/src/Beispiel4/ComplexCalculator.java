8129858c656dc6d1411f7eace67bbe7fe3023d9e
manfred nacktname <mansurgewinner@gmail.com>
2022-09-24 20:00:41 +0200

    ComplexrCalculator fertig

--- Hausuebung2/src/Beispiel4/ComplexCalculator.java
+++ Hausuebung2/src/Beispiel4/ComplexCalculator.java
@@ -7,21 +7,33 @@ public class ComplexCalculator extends AbstractCalculator{
 
     @Override
     public Number add(Number a, Number b) {
-        return null;
-    }
+        a.setB(a.getB() + b.getB());
+        a.setA(a.getA() + b.getA());
+
+        return a;    }
 
     @Override
     public Number subtract(Number a, Number b) {
-        return null;
-    }
+        a.setB(a.getB() - b.getB());
+        a.setA(a.getA() - b.getA());
+
+        return a;    }
 
     @Override
     public Number multiply(Number a, Number b) {
-        return null;
+        Number c = new Number();
+        c.setA((a.getA() * b.getA()) - (a.getB() * b.getB()));
+        c.setB((a.getA() * b.getB()) - (a.getB() * b.getA()));
+
+        return c;
     }
 
     @Override
     public Number divide(Number a, Number b) {
-        return null;
+        Number c = new Number();
+        c.setA(((a.getA() * b.getA()) + (a.getB() * b.getB())) / ((b.getA()*b.getA()) + (b.getB()*b.getB())));
+        c.setB(((a.getB() * b.getA()) - (a.getA() * b.getB())) / ((b.getA()*b.getA()) + (b.getB()*b.getB())));
+
+        return c;
     }
 }
