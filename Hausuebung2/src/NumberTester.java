fd50f075f577288c48127cffa5dae5b490e10777
manfred nacktname <mansurgewinner@gmail.com>
2022-09-24 09:12:55 +0200

    Datei erstellt zum testen

--- Hausuebung2/src/NumberTester.java
+++ Hausuebung2/src/NumberTester.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class NumberTester {
+public class NumberTester {
+        NumberTest oddTester;
+        NumberTest primeTester;
+        NumberTest palindromTester;
+
+        public NumberTester(String fileName)
+        {
+            //TODO
+        }
+
+        public void setOddEvenTester(NumberTest oddTester)
+        {
+                this.oddTester = oddTester;
+        }
+
+        public void setPrimeTester( NumberTest primeTester ) {
+                this.primeTester = primeTester;
+        }
+
+        public void setPalindromTester(NumberTest palindromTester) {
+                this.palindromTester = palindromTester;
+        }
+
+        public void testFile(){
+                //TODO
+        }
+
 }
