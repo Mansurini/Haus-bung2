ef6da715f20f5ff2965f0633bdce073703b1ef1c
manfred nacktname <mansurgewinner@gmail.com>
2022-09-24 19:29:21 +0200

    RationalCalculater fertig

--- Hausuebung2/src/Beispiel4/RationalCalculator.java
+++ Hausuebung2/src/Beispiel4/RationalCalculator.java
@@ -7,21 +7,44 @@ public class RationalCalculator extends AbstractCalculator{
 
     @Override
     public Number add(Number a, Number b) {
-        return null;
+        if (a.getB() == b.getB()) {
+            a.setA(a.getA() + b.getA());
+            return a;
+        }
+        Number c = new Number();
+        c.setB(a.getB() * b.getB());
+        c.setA((a.getA() * b.getB()) + (b.getA() * a.getB()));
+
+        return c;
     }
 
     @Override
     public Number subtract(Number a, Number b) {
-        return null;
+        if (a.getB() == b.getB()) {
+            a.setA(a.getA() - b.getA());
+            return a;
+        }
+        Number c = new Number();
+        c.setB(a.getB() * b.getB());
+        c.setA((a.getA() * b.getB()) - (b.getA() * a.getB()));
+
+        return c;
     }
 
     @Override
     public Number multiply(Number a, Number b) {
-        return null;
+        Number c = new Number();
+        c.setB(a.getB() * b.getB());
+        c.setA(a.getA() * b.getA());
+
+        return c;
     }
 
     @Override
     public Number divide(Number a, Number b) {
-        return null;
-    }
+        Number c = new Number();
+        c.setB(a.getB() * b.getA());
+        c.setA(a.getA() * b.getB());
+
+        return c;    }
 }
